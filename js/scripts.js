e29cecf94ec5d2ffa2d6a2c4a58150e86fee4fe7
Nick Kramer <krames12@gmail.com>
2016-03-05 17:04:21 -0500

    add / delete stramer functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ function getInfo(streamName){
 		success: function(data){
 			console.log(data);
 			if (data.status === 404){
-				$offline.append('<div class="stream-off stream-stat"><img src="http://publicdomainvectors.org/photos/mono-gnome-question.png" class="stream-icon"><p class="name">' + streamName + '</p><div class="stream-info"><p class="stream-info">Does Not Exist</p></div></div>');
+				$offline.append('<div class="stream-off stream-stat"><img src="http://publicdomainvectors.org/photos/mono-gnome-question.png" class="stream-icon"><p class="name">' + streamName + '</p><span class="delete-stream fa fa-times"></span><div class="stream-info"><p class="stream-info">Does Not Exist</p></div></div>');
 			} else if (data.stream == null){
 				offlineCall(streamName);
 			} else {
@@ -67,6 +67,8 @@ function addStream(streamName){
 	getInfo(streamer);
 }
 
-$('.delete-stream').click( function(){
+$('.stream-container').delegate('.delete-stream', 'click', function(){
+	$(this).closest('.stream-stat').remove();
+
 	console.log('delete');
 });
